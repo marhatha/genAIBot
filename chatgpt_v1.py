e026f9f4603d42a80e0e3879998d92fe9054a858
marhatha <puneet.marhatha@gmail.com>
2024-04-04 13:07:44 -0500

    v7

--- chatgpt_v1.py
+++ chatgpt_v1.py
@@ -1,7 +1,23 @@
 import random
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
 
-block = 8
-batch = 4
+# hyperparameters
+batch_size = 64 # how many independent sequences will we process in parallel?
+block_size = 256 # what is the maximum context length for predictions?
+max_iters = 5000
+eval_interval = 500
+learning_rate = 3e-4
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+eval_iters = 200
+n_embd = 384
+n_head = 6
+n_layer = 6
+dropout = 0.2
+# ------------
+#block = 8
+#batch = 4
 
 def readfile(file_path):
     with open(file_path, 'r', encoding='utf-8') as txt_file:
@@ -12,19 +28,19 @@ def readfile(file_path):
         for i in range(1, 1000):
             tokens.append(file[i-1])
         print(f"\nThe first 1000 characters are", tokens)
-    return file[:1000]
+#    return file[:1000]
+    return file
 
 def uniqchar(file_content):
     chars = sorted(list(set(file_content)))
     vocab_size = len(chars)
-    print(''.join(chars))
+#    print(''.join(chars))
     print(vocab_size)
     return chars
 
-
 def chartoint(file_content, stoi):
     int_list = encode(file_content, stoi)
-    print(f"\nThe integer list length is :", len(int_list))
+#    print(f"\nThe integer list length is :", len(int_list))
     l = len(int_list)
     n = int(0.9 * len(int_list))
     r = round(n)
@@ -39,9 +55,9 @@ def getbatch(list):
     rand = random.randint(0, 800)
     print(f"\nThe train_data list length is :", len(origlist))
     print(f"\nRandom number generated is :", rand)
-    for i in range(rand, (rand + block * batch), block):
-        context.append(origlist[i:i + block])
-        target.append(origlist[i + 1:i + 1 + block])
+    for i in range(rand, (rand + block_size * batch_size), block_size):
+        context.append(origlist[i:i + block_size])
+        target.append(origlist[i + 1:i + 1 + block_size])
     print(f"\nThe context list is :", context)
     print(f"\nThe target list is :", target)
     return context, target
@@ -66,7 +82,7 @@ encode = lambda s, stoi: [stoi[c] for c in s] # encoder: take a string, output a
 decode = lambda l, itos: ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
 
 train, val = chartoint(file_content, stoi)
-print(f"\ntraining data is ", train)
+#print(f"\ntraining data is ", train)
 xb, yb = getbatch(train)
 print(f"\nxb and yb is ", xb, yb)
 
